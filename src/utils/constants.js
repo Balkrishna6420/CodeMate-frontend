ce4d16ea6b246d387164ee0c3d4b09b50c21380d
Your GitHub Name <Your-GitHub-Email>
2026-08-18 00:24:32 +0530

    changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,4 @@
 export const BASE_URL =
   location.hostname === "localhost"
     ? "http://localhost:7777"
-    : location.hostname.includes("vercel.app")
-    ? "https://codemate-backend-hawh.onrender.com"
     : "/api";
